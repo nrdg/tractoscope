e48885266c6a5b92ca0f0ba2640437f6b7f6f652
Isaac Crane <isaaccrane@seattleacademy.org>
2023-05-17 09:47:29 -0700

    testing color code

--- callNiivue.js
+++ callNiivue.js
@@ -1,4 +1,4 @@
-function changeVolumeList(subjectData, opacityValue) {
+async function changeVolumeList(subjectData, opacityValue) {
     var volumeList1 = [
 	// first item is background image
 	 {
@@ -11,13 +11,21 @@ function changeVolumeList(subjectData, opacityValue) {
     var nv1 = new niivue.Niivue(({
   		show3Dcrosshair: true,//displays crosshair
   		backColor: [1, 1, 1, 1]}))//sets the background color.
-  	nv1.setSliceType(nv1.sliceTypeRender)//I don't know exactly what this does but when it is removed all three directions of slices are displayed
+  	nv1.setSliceType(nv1.sliceTypeRender)//all three directions of slices are displayed
   	nv1.attachTo('gl1')//attaches the entire image to the canvas.
     for (var q = 0; q<subjectData[1].length; q++){
       
-      nv1.loadMeshes([
+      	/*
+		nv1.loadMeshes([
     	 {url: subjectData[0][1][subjectData[1][q]], rgba255 : [0, 255, 0, 255],},
     	])//displays the fiber
+		*/
+		await nv1.loadMeshes([
+			{
+			  url: "./BrainMesh_ICBM152.lh.mz3",
+			  rgba255: [222, 164, 164, 255],
+			},
+		])
     }
   	
   	nv1.setClipPlane([-0.1, 270, 0])	//displays the clip plane. displaced in the x? direction by 0.1. rotated 270 degrees about the z axis. The third number clearly has to do with angle and initial position in the z direction but my experimentation is not leading me to a clear result.
