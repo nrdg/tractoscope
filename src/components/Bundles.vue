ed30d2f180f4521411c8a50046659dd2ec680cc9
Asa Gilmore <asaagilmore@gmail.com>
2023-07-20 12:28:12 -0700

    added mutliselect to bundles.vue

--- src/components/Bundles.vue
+++ src/components/Bundles.vue
@@ -1,44 +1,65 @@
 <script>
-export default {
-    props: {
-        bundleTypes: Array,
+    import Multiselect from '@vueform/multiselect'
 
-    },
-    data(){
-        return {
-            bundles: [],
-            showBoxes: false,
-            toggled: false
-        }
-    },
-    methods: {
-        displayBoxes(){
-            this.showBoxes=true
+    export default {
+        props: {
+            bundleTypes: Array,
+
         },
-        dontShowBoxes(){
-            this.showBoxes=false
-        },
-        update(){
-            this.colorsSelected = []
-            if (this.bundles.length < 24){
-                this.toggled = false
-            }
-            this.$emit("changedMesh", this.bundles)
-        },
-        toggle(){
-            this.bundles = []
-            if (this.toggled){
-                for (var bundleType in this.bundleTypes){
-                    this.bundles.push(parseInt(bundleType))
+        watch: {
+            bundleTypes(newVal) {
+                this.initBundles()
+            },
+            selectedBundles(newVal,oldVal){
+                if(newVal !== oldVal){
+                    this.emitBundles()
                 }
             }
-            this.update()
         },
+        data(){
+            return {
+                selectedBundles: null,
+                bundles: [{label:'select all', options:[]}],
+                showBoxes: false,
+                toggled: false
+            }
+        },
+        methods: {
+            emitBundles(){
+                console.log(this.selectedBundles)
+                this.colorsSelected = []
+                this.$emit("changedMesh", this.selectedBundles)
+            },
+            initBundles(){
+                const len = this.bundleTypes.length
+                for(var i = 0; i < len; i++){
+                    const bundle = this.bundleTypes[i]
+                    const option = {label: bundle.name, value: bundle.id}
+                    this.bundles[0].options.push(option)
+                }
+                console.log(this.bundles[0])
+            }
+        },
+        components:{
+            Multiselect,
+        }
     }
-}
 </script>
 
 <template>
+  <div>
+    <Multiselect :searchable="true"
+    placeholder="bundles"
+    mode='tags'
+    :groups="true"
+    v-model="selectedBundles"
+    :options="bundles" @change="emitBundles"
+    />
+  </div>
+</template>
+
+<style src="@vueform/multiselect/themes/default.css"></style>
+<!-- <template>
     <button v-if="!showBoxes" @click="displayBoxes" >Show Bundle Options</button>
     <div v-if="showBoxes">
         <button @click="dontShowBoxes">Don't Show Bundle Options</button>
@@ -55,4 +76,4 @@ export default {
             </tr>
         </table>
     </div>
-</template>
+</template> -->
