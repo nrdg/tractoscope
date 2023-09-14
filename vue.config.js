faa6012e6f1238cb4aa04187f977fb4b2da1a7a1
Asa Gilmore <asaagilmore@gmail.com>
2023-09-14 16:35:06 -0700

    updated vue.config.js to check for netlify

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/tractoscope'
-    : '/'
+let publicPath = '/tractoscope';
+
+if (process.env.NODE_ENV !== 'production') {
+    publicPath = '/';
+} else if (process.env.DEPLOY_ENV === 'netlify') {
+    publicPath = '/';
 }
+
+module.exports = {
+  publicPath: publicPath
+}
+
