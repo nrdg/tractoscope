c11ca55b92891716f82e7291ab289fe0da0de04a
Asa Gilmore <asaagilmore@gmail.com>
2023-12-30 14:55:02 -0600

    line spacing fix

--- src/utilites/logic.js
+++ src/utilites/logic.js
@@ -1,19 +1,19 @@
 //sorts filepaths by file extension
 //returns object with file extension as key paired with array of filepaths
 export function groupByExtension(filePaths) {
-    const filesByExtension = {};
-    for (const path of filePaths) {
-      const extensions = path.split('.').slice(1);
-      if (extensions.length > 0) {
-        const extension = extensions.join('.');
-        if (filesByExtension[extension]) {
-          filesByExtension[extension].push(path);
-        } else {
-          filesByExtension[extension] = [path];
-        }
+  const filesByExtension = {};
+  for (const path of filePaths) {
+    const extensions = path.split('.').slice(1);
+    if (extensions.length > 0) {
+      const extension = extensions.join('.');
+      if (filesByExtension[extension]) {
+        filesByExtension[extension].push(path);
+      } else {
+        filesByExtension[extension] = [path];
       }
     }
-    return filesByExtension;
+  }
+  return filesByExtension;
 }
 
 export function getLastPathComponent(filePath) {
