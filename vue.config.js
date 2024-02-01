3ccacde9a86a0c819d150769ba5a16bce89c659f
Asa Gilmore <asaagilmore@gmail.com>
2024-02-01 12:22:45 -0800

    added back vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+let publicPath = '/tractoscope';
+
+if (process.env.NODE_ENV !== 'production') {
+    publicPath = '/';
+} else if (process.env.DEPLOY_ENV === 'netlify') {
+    publicPath = '/';
+}
+
+module.exports = {
+  publicPath: publicPath
+}
